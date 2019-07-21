a40276f98cf6a55da1fe0462476c80077931f124
Nate K <me@nateserk.com>
2019-07-21 15:28:46 -0700

    Android: Added 'DetectionBasedTracker' for facial detection and allow CAMERA.

---
+++ app/src/main/java/com/nateserk/facedetection_sample/DetectionBasedTracker.java
@@ -0,0 +1,46 @@
+package com.nateserk.facedetection_sample;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+
+/**
+ * https://github.com/opencv/opencv/blob/master/samples/android/face-detection/src/org/opencv/samples/facedetect/DetectionBasedTracker.java
+ */
+public class DetectionBasedTracker
+{
+    public DetectionBasedTracker(String cascadeName, int minFaceSize) {
+        mNativeObj = nativeCreateObject(cascadeName, minFaceSize);
+    }
+
+    public void start() {
+        nativeStart(mNativeObj);
+    }
+
+    public void stop() {
+        nativeStop(mNativeObj);
+    }
+
+    public void setMinFaceSize(int size) {
+        nativeSetFaceSize(mNativeObj, size);
+    }
+
+    public void detect(Mat imageGray, MatOfRect faces) {
+        nativeDetect(mNativeObj, imageGray.getNativeObjAddr(), faces.getNativeObjAddr());
+    }
+
+    public void release() {
+        nativeDestroyObject(mNativeObj);
+        mNativeObj = 0;
+    }
+
+    private long mNativeObj = 0;
+
+    /** Below is where bridge between Native C/C++ and Java meets.*/
+
+    private static native long nativeCreateObject(String cascadeName, int minFaceSize);
+    private static native void nativeDestroyObject(long thiz);
+    private static native void nativeStart(long thiz);
+    private static native void nativeStop(long thiz);
+    private static native void nativeSetFaceSize(long thiz, int size);
+    private static native void nativeDetect(long thiz, long inputImage, long faces);
+}
