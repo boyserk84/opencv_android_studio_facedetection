790991c65fd416eb9cb67a33247b2d6a3cc2ddc3
Nate K <me@nateserk.com>
2019-07-21 20:24:30 -0700

    JNI Modify method signature to match the new package name.

--- app/jni/DetectionBasedTracker_jni.h
+++ app/jni/DetectionBasedTracker_jni.h
@@ -2,7 +2,7 @@
 // Created by Nate K on 2019-07-21.
 //
 #include <jni.h>
-/* Header for class com_nateserk_facedetection_sample_DetectionBasedTracker */
+/* Header for class com_nateserk_facetracker_DetectionBasedTracker */
 
 #ifndef FACEDETECTION_DETECTIONBASEDTRACKER_JNI_H
 #define FACEDETECTION_DETECTIONBASEDTRACKER_JNI_H
@@ -10,51 +10,51 @@
 extern "C" {
 #endif
 /*
- * Class:     com_nateserk_facedetection_sample_DetectionBasedTracker
+ * Class:     com_nateserk_facetracker_DetectionBasedTracker
  * Method:    nativeCreateObject
  * Signature: (Ljava/lang/String;F)J
  */
-JNIEXPORT jlong JNICALL Java_com_nateserk_facedetection_sample_DetectionBasedTracker_nativeCreateObject
+JNIEXPORT jlong JNICALL Java_com_nateserk_facetracker_DetectionBasedTracker_nativeCreateObject
   (JNIEnv *, jclass, jstring, jint);
 
 /*
- * Class:     com_nateserk_facedetection_sample_DetectionBasedTracker
+ * Class:     com_nateserk_facetracker_DetectionBasedTracker
  * Method:    nativeDestroyObject
  * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_com_nateserk_facedetection_sample_DetectionBasedTracker_nativeDestroyObject
+JNIEXPORT void JNICALL Java_com_nateserk_facetracker_DetectionBasedTracker_nativeDestroyObject
   (JNIEnv *, jclass, jlong);
 
 /*
- * Class:     com_nateserk_facedetection_sample_DetectionBasedTracker
+ * Class:     com_nateserk_facetracker_DetectionBasedTracker
  * Method:    nativeStart
  * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_com_nateserk_facedetection_sample_DetectionBasedTracker_nativeStart
+JNIEXPORT void JNICALL Java_com_nateserk_facetracker_DetectionBasedTracker_nativeStart
   (JNIEnv *, jclass, jlong);
 
 /*
- * Class:     com_nateserk_facedetection_sample_DetectionBasedTracker
+ * Class:     com_nateserk_facetracker_DetectionBasedTracker
  * Method:    nativeStop
  * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_com_nateserk_facedetection_sample_DetectionBasedTracker_nativeStop
+JNIEXPORT void JNICALL Java_com_nateserk_facetracker_DetectionBasedTracker_nativeStop
   (JNIEnv *, jclass, jlong);
 
   /*
-   * Class:     com_nateserk_facedetection_sample_DetectionBasedTracker
+   * Class:     com_nateserk_facetracker_DetectionBasedTracker
    * Method:    nativeSetFaceSize
    * Signature: (JI)V
    */
-  JNIEXPORT void JNICALL Java_com_nateserk_facedetection_sample_DetectionBasedTracker_nativeSetFaceSize
+  JNIEXPORT void JNICALL Java_com_nateserk_facetracker_DetectionBasedTracker_nativeSetFaceSize
   (JNIEnv *, jclass, jlong, jint);
 
 /*
- * Class:     com_nateserk_facedetection_sample_DetectionBasedTracker
+ * Class:     com_nateserk_facetracker_DetectionBasedTracker
  * Method:    nativeDetect
  * Signature: (JJJ)V
  */
-JNIEXPORT void JNICALL Java_com_nateserk_facedetection_sample_DetectionBasedTracker_nativeDetect
+JNIEXPORT void JNICALL Java_com_nateserk_facetracker_DetectionBasedTracker_nativeDetect
   (JNIEnv *, jclass, jlong, jlong, jlong);
 
 #ifdef __cplusplus
